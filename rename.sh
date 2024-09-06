6041e5d472c032aaa933b4dd29b7d6d725f1444a
ErnstRoell <ernstroell@gmail.com>
2024-09-06 12:25:38 +0200

    Cleaning

--- rename.sh
+++
@@ -1,9 +0,0 @@
-folders=$(ls ShapeNetCore.v2.PC15k/)
-
-for folder in $folders
-do
-  cd /mnt/c/users/ernst/Documents/02-Work/06-Repos/PointFlow/data
-  cd ./ShapNetCore.v2.PC15k/$folder/_
-  mv * ../
-done
-
